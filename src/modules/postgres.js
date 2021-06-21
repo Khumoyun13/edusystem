7354d80804b2abb03fbe9e473b0cb21066c55bcb
Khumoyun13 <mirzayevhumoyun5plus@gmail.com>
2021-06-21 17:43:38 +0500

    Add postgres and configs

---
+++ src/modules/postgres.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+import config from "../config.js";
+
+import UserModel from "../models/UserModel.js";
+
+const sequelize = new Sequelize(config.PG_CONNECTION_STRING, {
+  logging: false,
+});
+
+async function postgres() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+
+    let db = {};
+    db.users = await UserModel(Sequelize, sequelize);
+    await sequelize.sync({ force: true });
+    return db;
+  } catch (e) {
+    console.error("Unable to connect to the database:", e);
+  }
+};
+
+export default postgres;
---
+++ src/server.js
@@ -0,0 +1,33 @@
+// MODULES - start  //
+import express from "express";
+import http from "http";
+import fs from "fs";
+import path from "path";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+// -- end //
+
+// CUSTOM MODULES - start //
+import postgres from "./modules/postgres.js";
+// -- end //
+
+async function main() {
+  let db = await postgres();
+
+  let __dirname = path.resolve(path.dirname(""));
+
+  const app = express();
+  const server = http.createServer(app);
+  server.listen(3300, () => console.log(`SERVER READY`));
+
+  app.use(cors());
+  app.use(helmet());
+  app.use(morgan("dev"));
+  app.use(async (req, res, next) => {
+    req.postgres = db;
+    next();
+});
+}
+
+main();
